01f1e7676cfef754e45c32850ab8858867c5ec02
jeweleries <manoorani98@gmail.com>
2025-08-22 00:17:10 +0500

    Update bracelets.js

--- bracelets.js
+++ bracelets.js
@@ -9,6 +9,56 @@ const bracelets = [
         image: "Cartier Bracelet.jpg",
         price: "Rs 1499"
     },
+    {
+        name: "Black Flower Bracelet - Set Of 3",
+        image: "Black Flower Bracelet - Set Of 3.jpg",
+        price: "Rs 649"
+    },
+    {
+        name: "Multi Color Diamond Bracelet",
+        image: "Multi Color Diamond Bracelet.jpg",
+        price: "Rs 499"
+    },
+    {
+        name: "Rainbow Luxe Bracelet",
+        image: "Rainbow Luxe Bracelet.jpg",
+        price: "Rs 999"
+    },
+    {
+        name: "Multi Gem Bracelet",
+        image: "Multi Gem Bracelet.jpg",
+        price: "Rs 1299"
+    },
+    {
+        name: "Heart Bracelet",
+        image: "Heart Bracelet.jpg",
+        price: "Rs 1199"
+    },
+    {
+        name: "Gold Steel Bracelet Combo",
+        image: "Gold Steel Bracelet Combo.jpg",
+        price: "Rs 1499"
+    },
+    {
+        name: "Diamond Studded Gold Bracelet",
+        image: "Diamond Studded Gold Bracelet.jpg",
+        price: "Rs 699"
+    },
+    {
+        name: "Diamond Studded Gold Bracelet",
+        image: "Diamond Studded Gold Bracelet.jpg",
+        price: "Rs 699"
+    },
+    {
+        name: "White Flower Bracelet",
+        image: "Van Cleef Bracelet White.jpg",
+        price: "Rs 1050"
+    }
+    
+    
+   
+   
+    
     
 ];
 
